4774e63fe7fc17ded2fc5a21c9b881faf9d2ed7d
dju <dju@djuland.net>
2021-01-17 09:47:37 +0100

    section 02 goroutines 5 exercice hello

--- 01-exercise/01-goroutines/01-hello/main.go
+++ 01-exercise/01-goroutines/01-hello/main.go
@@ -19,12 +19,16 @@ func main() {
 	// TODO: write goroutine with different variants for function call.
 
 	// goroutine function call
-
+	go fun("Go routine 01")
 	// goroutine with anonymous function
-
+	go func() {
+		fun("Go routine 02")
+	}()
 	// goroutine with function value call
-
+	fv := fun
+	go fv("Go routine 03")
 	// wait for goroutines to end
-
+	fmt.Println("waiting for goroutines to complete..")
+	time.Sleep(3 * time.Second)
 	fmt.Println("done..")
 }
